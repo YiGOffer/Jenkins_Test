85d9c8ea5813095bf5ec2aad591d62706c1e78e9
YiGOffer <yyjshang@163.com>
2022-12-29 15:46:46 +0800

    add Python sh

---
+++ runPython.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+REPLACE_EXECUTE_PATH=$WORKSPACE/$GIT_CHECKOUT_DIR/ReplaceConfig.sh
+PACKAGER_EXECUTE_PATH=$WORKSPACE/$GIT_CHECKOUT_DIR/packaging/Packager.py
+PACKING_CONFIG_PATH=$WORKSPACE/$GIT_CHECKOUT_DIR/packaging/Config.json
+RELEASE_PATH=$WORKSPACE/$GIT_CHECKOUT_DIR/release_configs
+CONFIGS_PATH=$WORKSPACE/$GIT_CHECKOUT_DIR/packaging/res/data/dacsagent/bin/configs
+README=$WORKSPACE/$GIT_CHECKOUT_DIR/release_configs/README.md
+SUCCEED="\e[32m Success!\e[39m"
+
+# 设置$USER
+export USER=root
+# 根据公司名替换configs
+if [ -d $RELEASE_PATH/$business/configs ] ; then
+    cp -r $RELEASE_PATH/$business/configs/* $CONFIGS_PATH/
+    echo -e "cp -r $RELEASE_PATH/$business/configs/* $SUCCEED"
+    cp ${README} ${CONFIGS_PATH}/
+    echo -e "cp ${README} $SUCCEED"
+else
+    echo -e "$RELEASE_PATH/$business/configs does not \e[31mexist \e[39m !";exit 1;
+fi
+# 执行打包
+if [ ! -f "$PACKAGER_EXECUTE_PATH" ]; then
+	if [ ! -x $PACKAGER_EXECUTE_PATH ]; then
+        chmod a+x $PACKAGER_EXECUTE_PATH;
+	fi
+    if [ ! -f "$PACKING_CONFIG_PATH"]; then
+        $WORKSPACE/$GIT_CHECKOUT_DIR/packaging/Packager.py
+    fi
+    $WORKSPACE/$GIT_CHECKOUT_DIR/packaging/Packager.py
+else 
+	echo "$PACKAGER_EXECUTE_PATH not exist!"; exit 1;
+fi
